4687009c648119720ee1a2f4819a5abccad895a6
Swart-G <rametonter@gmail.com>
2026-09-08 18:47:21 +0300

    fix(android): enable generated BuildConfig

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,10 @@ android {
     compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
