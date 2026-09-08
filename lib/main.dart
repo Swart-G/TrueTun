ae62a7104f94e9cee15df62b939f35642429f9d9
Swart-G <rametonter@gmail.com>
2026-09-08 18:32:11 +0300

    feat(android): launch native-focused mobile shell

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tray_manager/tray_manager.dart' as tray;
+import 'package:truetun/src/android_app.dart';
 import 'package:truetun/src/app.dart';
 import 'package:truetun/src/application/app_state.dart';
 import 'package:truetun/src/core/core_adapter.dart';
@@ -18,7 +19,9 @@ Future<void> main(List<String> arguments) async {
     ProviderScope(
       child: _DesktopLifecycle(
         startHidden: arguments.contains('--hidden'),
-        child: const TrueTunApp(),
+        child: Platform.isAndroid
+            ? const TrueTunAndroidApp()
+            : const TrueTunApp(),
       ),
     ),
   );
